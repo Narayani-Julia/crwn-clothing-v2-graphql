89a73589d6e39c47af5a780791951c66674a813d
Nara <jmnarayani@gmail.com>
2025-07-25 12:56:02 -0400

    added graphQL and Apollo client to project

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ import App from './App';
 import { UserProvider } from './contexts/user.context';
 import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
-
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client'
 import './index.scss';
 
 const rootElement = document.getElementById('root');
-
+const client = new ApolloClient({
+  uri: 'https://crwn-clothing.com/',
+  cache: new InMemoryCache(),
+});
 render(
   <React.StrictMode>
+  <ApolloProvider client={client}>
     <BrowserRouter>
       <UserProvider>
         <CategoriesProvider>
@@ -22,6 +26,7 @@ render(
         </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>,
   rootElement
 );
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,20 +1,45 @@
 import { useContext, useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
-
+import {gql} from '@apollo/client';
 import ProductCard from '../../components/product-card/product-card.component';
 
 import { CategoriesContext } from '../../contexts/categories.context';
 
 import { CategoryContainer, Title } from './category.styles';
+import { useQuery } from '@apollo/client';
 
+const GET_CATEGORY = gql`
+query($title: String!){
+  getCollectionsByTitle(title: $title){
+  id
+  title
+  items{
+    id
+    name
+    price
+    imageUrl
+    }
+  }
+}
+`
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
-  const [products, setProducts] = useState(categoriesMap[category]);
+  const {loading, error, data} = useQuery(GET_CATEGORY, {
+    variables: {
+      title: category
+    },
+  });
+  const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    setProducts(categoriesMap[category]);
-  }, [category, categoriesMap]);
+    if(data){
+      const{
+        getCollectionsByTitle: {items},
+
+      } = data;
+      setProducts(items);
+    }
+  }, [category, data]);
 
   return (
     <Fragment>
